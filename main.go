346c645b318b221c502b4f3273386cbeec31d067
haya14busa <hayabusa1419@gmail.com>
2016-08-28 09:16:22 +0900

    implement vim-server

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
@@ -14,7 +14,11 @@ var (
 	port = flag.Int("port", 8765, "The server port")
 )
 
-var vimCon = make(chan net.Conn, 1)
+type myHandler struct{}
+
+func (h *myHandler) Serve(w io.Writer, msg *Message) {
+	log.Printf("receive: %#v", msg)
+}
 
 func main() {
 	flag.Parse()
@@ -27,7 +31,8 @@ func main() {
 		log.Fatal(err)
 	}
 	defer l.Close()
-	go serve(l)
+	server := &Server{Handler: &myHandler{}}
+	go server.Serve(l)
 
 	p, err := NewVimServer(addr)
 	if err != nil {
@@ -36,74 +41,13 @@ func main() {
 	defer p.Close()
 
 	scanner := bufio.NewScanner(os.Stdin)
-	con := <-vimCon
-	defer con.Close()
+	conn := server.Connect()
+	defer conn.Close()
 	log.Println("connected to vim server!")
 	for scanner.Scan() {
 		log.Printf("send: %v", scanner.Text())
-		if _, err := con.Write(scanner.Bytes()); err != nil {
+		if _, err := conn.Write(scanner.Bytes()); err != nil {
 			log.Println(err)
 		}
 	}
-}
-
-func serve(l net.Listener) {
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-
-		c, ok := conn.(*net.TCPConn)
-		if !ok {
-			log.Println(err)
-			continue
-		}
-
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		// go handleConn(conn)
-		go handleConn(c)
-	}
-}
-
-func handleConn(c net.Conn) {
-	// defer c.Close()
-	vimCon <- c
-
-	fmt.Printf("local addr: %v\n", c.LocalAddr())
-	fmt.Printf("remote addr: %v\n", c.RemoteAddr())
-
-	scanner := bufio.NewScanner(c)
-	for scanner.Scan() {
-		log.Printf("receive: %v", scanner.Text())
-		msgID, expr, err := unmarshalMsg(scanner.Bytes())
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		log.Println(msgID, expr)
-	}
-	if err := scanner.Err(); err != nil {
-		log.Println(err)
-	}
-}
-
-// unmarshalMsg unmarshals json message from Vim.
-// msg format: [{number},{expr}]
-func unmarshalMsg(data []byte) (msgID int, expr interface{}, err error) {
-	var vimMsg [2]interface{}
-	if err := json.Unmarshal(data, &vimMsg); err != nil {
-		return 0, nil, fmt.Errorf("fail to decode vim message: %v", err)
-	}
-	number, ok := vimMsg[0].(float64)
-	if !ok {
-		return 0, nil, fmt.Errorf("expects message ID, but got %+v", vimMsg[0])
-	}
-	msgID = int(number)
-	expr = vimMsg[1]
-	return msgID, expr, nil
 }
---
+++ server.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+type Message struct {
+	MsgID int
+	Body  interface{}
+}
+
+type Handler interface {
+	Serve(io.Writer, *Message)
+}
+
+type Server struct {
+	Handler Handler // handler to invoke
+
+	conn chan net.Conn
+}
+
+func (srv *Server) Serve(l net.Listener) error {
+	defer l.Close()
+	srv.conn = make(chan net.Conn, 1)
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+		if err != nil {
+			if ne, ok := err.(net.Error); ok && ne.Temporary() {
+				log.Println(err)
+				continue
+			}
+			return err
+		}
+
+		srv.conn <- conn
+
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go srv.handleConn(conn)
+	}
+	return nil
+}
+
+func (srv *Server) Connect() net.Conn {
+	return <-srv.conn
+}
+
+// Serve a new connection.
+func (srv *Server) handleConn(c net.Conn) {
+	scanner := bufio.NewScanner(c)
+	for scanner.Scan() {
+		msg, err := unmarshalMsg(scanner.Bytes())
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		srv.Handler.Serve(c, msg)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+}
+
+// unmarshalMsg unmarshals json message from Vim.
+// msg format: [{number},{expr}]
+func unmarshalMsg(data []byte) (msg *Message, err error) {
+	var vimMsg [2]interface{}
+	if err := json.Unmarshal(data, &vimMsg); err != nil {
+		return nil, fmt.Errorf("fail to decode vim message: %v", err)
+	}
+	number, ok := vimMsg[0].(float64)
+	if !ok {
+		return nil, fmt.Errorf("expects message ID, but got %+v", vimMsg[0])
+	}
+	m := &Message{
+		MsgID: int(number),
+		Body:  vimMsg[1],
+	}
+	return m, nil
+}
